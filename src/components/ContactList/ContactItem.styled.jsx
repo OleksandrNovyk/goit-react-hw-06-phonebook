8358c160ce980b0dbe150204bf9355ee66cc9bef
OleksandrNovyk <novykav@gmail.com>
2023-06-12 12:39:18 +0300

    1

---
+++ src/components/ContactList/ContactItem.styled.jsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const ListItem = styled.li`
+  min-width: 50vw;
+  display: flex;
+  font-weight: bold;
+  justify-content: space-between;
+  align-items: center;
+  font-size: 20px;
+  padding: 32px;
+  border-radius: 12px;
+  background-color: hsla(215, 98%, 79%, 0.1);
+  border: 2px solid blanchedalmond;
+`;
+
+export const ListItemData = styled.p`
+  font-size: 24px;
+  margin-right: 36px;
+`;
